6d5111d5f7df318530c2112f1211703e21557b01
delusion8399 <delusion8399@gmail.com>
2025-08-09 00:43:57 +0530

    fix: use correct production env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,12 @@
 const hosts = {
   development: "http://localhost:8000",
-  production: "https://pdf.maxxfibernet.in",
+  production: "https://pdfninja-api.sham1.dev",
 };
 
 export const config = {
-  apiBaseUrl: hosts[process.env.NODE_ENV as keyof typeof hosts] || "http://localhost:8080",
+  apiBaseUrl:
+    hosts[process.env.NODE_ENV as keyof typeof hosts] ||
+    "http://localhost:8080",
   isDevelopment: process.env.NODE_ENV === "development",
   isProduction: process.env.NODE_ENV === "production",
 };
